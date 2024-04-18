518cd1b8098c76e89bcd16818a2dad684c92637a
aroslav <xl5002xf@gmail.com>
2024-04-18 18:53:07 +0500

    Add -bench

--- src/main/java/ru/yoricya/ysc/Main.java
+++ src/main/java/ru/yoricya/ysc/Main.java
@@ -4,24 +4,38 @@ import ru.yoricya.ysc.Modules.LangFileUtils;
 
 public class Main {
     public static void main(String[] args) {
-        boolean is = false;
+        long startTime = System.currentTimeMillis();
+
+        boolean isBench = false;
+
         for(int i = 0; i<args.length; i++){
             if(args[i].equals("-yscp") && args.length-i > 1){
-                is = true;
                 Ysc ysc = new Ysc().loadProjectDir(args[i+1]);
                 Object w = ysc.runMain(args);
-                if(w instanceof Integer) System.exit((int) w);
+
+                if(isBench)
+                    System.err.println("[INTERPRETER] Script success ended by "+(System.currentTimeMillis()-startTime)+"ms");
+
+                if(w instanceof Integer)
+                    System.exit((int) w);
+                else
+                    System.exit(0);
             }else if(args[i].equals("-ysc") && args.length-i > 1){
-                is = true;
                 Ysc ysc = new Ysc();
                 String script = LangFileUtils.readFile(args[i+1]);
+
+                if(isBench)
+                    System.err.println("[INTERPRETER] Script success ended by "+(System.currentTimeMillis()-startTime)+"ms");
+
                 System.exit(ysc.parse(script));
+            }else if(args[i].equals("-bench") && args.length-i > 1){
+                isBench = true;
             }
         }
 
-        if(!is){
-            System.out.println("Usage: -yscp <path to project directory>");
-            System.out.println("or: -ysc <path to .ysc file>");
-        }
+        System.out.println("Ysc interpreter. "+Ysc.YSC_VERSION);
+        System.out.println("Usage: -bench(Optional) -yscp <path to project directory>");
+        System.out.println("or: -bench(Optional) -ysc <path to .ysc file>");
+
     }
 }
